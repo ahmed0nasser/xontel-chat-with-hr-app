c32a23a8e95abda697a11ddc8eb2bb500589afb0
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-10-29 00:45:46 +0300

    Create bottom tabs layout

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Tabs } from "expo-router";
+import { Home, User } from "lucide-react-native";
+import { theme } from "@/utils/theme";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.textSecondary,
+        tabBarStyle: {
+          height: "auto",
+          backgroundColor: theme.colors.surface,
+          borderTopColor: theme.colors.border,
+          borderTopWidth: 1,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ size, color }) => <Home size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ size, color }) => <User size={size} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
